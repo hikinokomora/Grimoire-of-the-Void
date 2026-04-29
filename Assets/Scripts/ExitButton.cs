7eaf5fbb2832f8e7b28a8e9ade43bb4e4789e559
pavelbelousov139 <69257066+pavelbelousov139@users.noreply.github.com>
2026-04-29 13:24:14 +0300

    ExitButton

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("🚪 Выход из игры...");
+#if UNITY_EDITOR
+        // В редакторе просто останавливаем Play Mode
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // В билде закрываем приложение
+        Application.Quit();
+#endif
+    }
+}
